2ba44277c83fd918331ebccff89fe6f6bc0dd376
Subham777-max <subhamray865@gmail.com>
2026-08-07 11:58:07 +0530

    fix(app): reorder import statements for chat routes

--- backend/modules/chat/src/app.js
+++ backend/modules/chat/src/app.js
@@ -1,11 +1,11 @@
 import express from "express";
 import morgan from "morgan";
-
+import chatRoutes from "./routes/chat.routes.js";
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+app.use("/", chatRoutes);
 export default app;
